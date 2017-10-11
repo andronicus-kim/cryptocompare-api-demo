57e4b948e55041c8bf29dd50c620c5c565ece2e3
andronicus-kim <androkim.kim7@gmail.com>
2017-10-11 10:52:07 +0300

    adds basepresenter and baseview

---
+++ app/src/main/java/kim/andronicus/cryptoasus/conversion/ConversionFragment.java
@@ -0,0 +1,30 @@
+package kim.andronicus.cryptoasus.conversion;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kim.andronicus.cryptoasus.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ConversionFragment extends Fragment {
+
+
+    public ConversionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.conversion_fragment, container, false);
+    }
+
+}
